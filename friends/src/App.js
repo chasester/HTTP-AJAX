c5213bb45d5d27ff5d39755625a397069d7aba14
Chase Wex <clapingmanly@hotmail.com>
2019-05-08 12:47:44 -0400

    Removed fluff and added basic structure of app, added needed libraries

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components'
+import axios from 'axios';
+import FriendsList from './Components/FriendsList';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const HorizontalBar = styled.div `
+  border: 1px solid black;
+  width: 100%;
+  content: "hello";
+`
+
+class App extends React.Component{
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount()
+  {
+    axios
+    .get("http://localhost:5000/friends")
+    .then(resault=>{
+      this.setState({friends: resault.data})
+    })
+    .catch( err=>{
+      console.log(err);
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>FRIENDS LIST</h1>
+        <HorizontalBar/>
+        <FriendsList />
+      </div>
+    );
+  }
 }
 
 export default App;
