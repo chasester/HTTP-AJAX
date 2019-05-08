520b67000e4f4621cbb26b7eb5c55088842cda8f
Chase Wex <clapingmanly@hotmail.com>
2019-05-08 15:34:14 -0400

    Added name chage to help clarify data

--- friends/src/Components/FriendCard.js
+++ friends/src/Components/FriendCard.js
@@ -30,16 +30,17 @@ class FriendCard extends React.Component
         }
     }
 
-    onChange()
+    onSubmit()
     {
         let obj =
         {
             id: this.props.friend.id,
             name: this.state.textbox === 0 ? this.state.value : this.props.friend.name,
-            age: this.state.textbox === 1 ? this.state.value : this.props.friend.age,
+            age: this.state.textbox === 1 ? parseInt(this.state.value) : this.props.friend.age,
             email: this.state.textbox === 2 ? this.state.value : this.props.friend.email, 
         }
-        this.props.changeCb(obj);
+        if(obj.name === this.props.friend.name && obj.age === this.props.friend.age && obj.email === this.props.friend.email);
+        else this.props.changeCb(obj);
         this.setState({textbox: -1, value: ""});
     }
     render()
@@ -47,9 +48,9 @@ class FriendCard extends React.Component
 
         return(
             <FriendContainer>
-                    {this.state.textbox !== 0 ? <Name onClick={()=>this.setState({textbox: 0, value: this.props.friend.name})}>{this.props.friend.name}</Name> : <Forum onBlur={() => this.onChange()} onChange={event => this.setState({textbox: 0, value: event.target.value})} value={this.state.value} />}
-                    <Age>{this.state.textbox !== 1 ? <Age onClick={()=>this.setState({textbox: 1, value: this.props.friend.age})}>{this.props.friend.age}</Age> : <Forum onBlur={() => this.onChange() } onChange={event => this.setState({textbox: 1, value: event.target.value})} value={this.state.value} />}</Age>
-                    <Contact>{this.state.textbox !== 2 ? <Contact onClick={()=>this.setState({textbox: 2, value: this.props.friend.email})}>{this.props.friend.email}</Contact> : <Forum onBlur={() => this.onChange() } onChange={event => this.setState({textbox: 2, value: event.target.value})} value={this.state.value} />}</Contact>
+                    {this.state.textbox !== 0 ? <Name onClick={()=>this.setState({textbox: 0, value: this.props.friend.name})}>{this.props.friend.name}</Name> : <Forum onBlur={() => this.onSubmit()} onChange={event => this.setState({textbox: 0, value: event.target.value})} value={this.state.value} />}
+                    <Age>{this.state.textbox !== 1 ? <Age onClick={()=>this.setState({textbox: 1, value: this.props.friend.age})}>{this.props.friend.age}</Age> : <Forum onBlur={() => this.onSubmit() } onChange={event => this.setState({textbox: 1, value: event.target.value})} value={this.state.value} />}</Age>
+                    <Contact>{this.state.textbox !== 2 ? <Contact onClick={()=>this.setState({textbox: 2, value: this.props.friend.email})}>{this.props.friend.email}</Contact> : <Forum onBlur={() => this.onSubmit() } onChange={event => this.setState({textbox: 2, value: event.target.value})} value={this.state.value} />}</Contact>
                     <button onClick={()=> this.props.deleteCb(this.props.friend.id)}>X</button>
             </FriendContainer> 
         );
